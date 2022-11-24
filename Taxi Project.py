9b920ab85e13f2c16d32b366203badf06f572dd4
CIS9590DealFinder <117138408+CIS9590DealFinder@users.noreply.github.com>
2022-11-23 21:25:15 -0500

    Add files via upload

--- Taxi Project.py
+++ Taxi Project.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-import pandas as pd
+import modin.pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -129,8 +129,6 @@ df['hour'] = df['lpep_pickup_datetime'].dt.hour
 days= ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 plt.figure(figsize= (20, 10))
 Day = df.groupby('week')['PULocationID'].count().reindex(days).reset_index()
-#sns.countplot(y= 'week', data= df)
-#plt.ylabel('Day of Week')
 sns.set_style("darkgrid")
 
 plt.figure(figsize=(20,10))
@@ -148,10 +146,10 @@ plt.show()
 
 
 plt.figure(figsize= (20, 10))
-ax = sns.countplot(x= 'hour', data= df,color='royalblue')
+ax1 = sns.countplot(x= 'hour', data= df,color='royalblue')
 
-ax.set_ylabel("Number of Rides",fontsize=18)
-ax.set_xlabel("Hour of Day",fontsize=18)
+ax1.set_ylabel("Number of Rides",fontsize=18)
+ax1.set_xlabel("Hour of Day",fontsize=18)
 plt.title('Number of Rides by the Hour', fontsize=20)
 
 plt.savefig("Rides_Hour.png",dpi=300)
@@ -175,10 +173,10 @@ tip_pct_week = df.groupby('week')['tip_pct'].mean().reindex(days).reset_index()
 
 
 plt.figure(figsize= (20, 10))
-ax = sns.barplot(x='week',y='tip_pct',data=tip_pct_week)
+ax2 = sns.barplot(x='week',y='tip_pct',data=tip_pct_week)
 
-ax.set_ylabel("Percent of Tip",fontsize=18)
-ax.set_xlabel("Day of Week",fontsize=18)
+ax2.set_ylabel("Percent of Tip",fontsize=18)
+ax2.set_xlabel("Day of Week",fontsize=18)
 plt.title('Average Tip Percent by Day', fontsize=20)
 
 plt.savefig("Tip_Day.png",dpi=300)
@@ -191,10 +189,10 @@ plt.show()
 
 plt.figure(figsize= (20, 10))
 tip_pct_hour = df.groupby('hour')['tip_pct'].mean().reset_index()
-ax = sns.barplot(x='hour',y='tip_pct',data=tip_pct_hour,color='royalblue')
+ax3 = sns.barplot(x='hour',y='tip_pct',data=tip_pct_hour,color='royalblue')
 
-ax.set_ylabel("Percent of Tip",fontsize=18)
-ax.set_xlabel("Hour of Day",fontsize=18)
+ax3.set_ylabel("Percent of Tip",fontsize=18)
+ax3.set_xlabel("Hour of Day",fontsize=18)
 plt.title('Average Tip Percent by Hour', fontsize=20)
 
 plt.savefig("Tip_Hour.png",dpi=300)
