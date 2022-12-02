e82c4358872f286a33b94495127f8a3fa057c8c0
CIS9590DealFinder <117138408+CIS9590DealFinder@users.noreply.github.com>
2022-12-01 22:43:48 -0500

    Update inteloneapi.sh

--- inteloneapi.sh
+++ inteloneapi.sh
@@ -1,11 +1,8 @@
 rm DealFinder
 mkdir DealFinder
 cd DealFinder
-
 wget https://github.com/CIS9590DealFinder/DealFinder/raw/main/taxi_tripdata.csv
 wget https://raw.githubusercontent.com/CIS9590DealFinder/DealFinder/main/TaxiProject.py
-
 pip install seaborn
-
 ipython TaxiProject.py
 ls
