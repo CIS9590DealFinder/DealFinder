42fab6485ab58fc77c12d3d87ad3134c11b14300
CIS9590DealFinder <117138408+CIS9590DealFinder@users.noreply.github.com>
2022-12-06 15:00:04 -0500

    Update TaxiProject.py

--- TaxiProject.py
+++ TaxiProject.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-#importing depedencies
+#importing dependicies
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -27,28 +27,34 @@ df['week'] = df['lpep_pickup_datetime'].dt.day_name()
 df['hour'] = df['lpep_pickup_datetime'].dt.hour
 days= ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 Day = df.groupby('week')['PULocationID'].count().reindex(days).reset_index()
-#data visualization
 sns.set_style("darkgrid")
+#data visualization
 ax = sns.barplot(x="week", y="PULocationID",data=Day)
 ax.set_ylabel("Number of Rides",fontsize=18)
 ax.set_xlabel("Day of Week",fontsize=18)
 plt.title('Number of Rides by Day', fontsize=20)
 plt.savefig("Rides_Day.png",dpi=300)
+plt.show()
+
 ax1 = sns.countplot(x= 'hour', data= df,color='royalblue')
 ax1.set_ylabel("Number of Rides",fontsize=18)
 ax1.set_xlabel("Hour of Day",fontsize=18)
 plt.title('Number of Rides by the Hour', fontsize=20)
 plt.savefig("Rides_Hour.png",dpi=300)
+plt.show()
+
 tip_pct_week = df.groupby('week')['tip_pct'].mean().reindex(days).reset_index()
 ax2 = sns.barplot(x='week',y='tip_pct',data=tip_pct_week)
 ax2.set_ylabel("Percent of Tip",fontsize=18)
 ax2.set_xlabel("Day of Week",fontsize=18)
 plt.title('Average Tip Percent by Day', fontsize=20)
 plt.savefig("Tip_Day.png",dpi=300)
+plt.show()
+
 tip_pct_hour = df.groupby('hour')['tip_pct'].mean().reset_index()
 ax3 = sns.barplot(x='hour',y='tip_pct',data=tip_pct_hour,color='royalblue')
 ax3.set_ylabel("Percent of Tip",fontsize=18)
 ax3.set_xlabel("Hour of Day",fontsize=18)
 plt.title('Average Tip Percent by Hour', fontsize=20)
 plt.savefig("Tip_Hour.png",dpi=300)
-
+plt.show()
